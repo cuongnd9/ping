e7fd2e5913dfaae42940fd1414449e99dea0b979
Nguyen Duy Cuong <cuongw.me@gmail.com>
2019-10-01 16:51:29 +0700

    Setup basic server & cron

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const cron = require('cron');
 
 const app = express();
+const { CronJob } = cron;
 
-app.get('/', (_, res) => res.send('Xin chào 👋'))
+const job = new CronJob('0 */29 9-1 * * *', () => {
+  console.log('ping..........');
+});
+
+job.start();
+
+app.get('/', (_, res) => res.send('Xin chào 👋'));
 
 app.listen(9000, () => console.log('Server is running on port 9000'));
